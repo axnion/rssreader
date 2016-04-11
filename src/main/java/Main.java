9bc963539a0a7176474705e900dff3c3525d1e85
Axel Nilsson <an222yp@student.lnu.se>
2016-04-11 04:25:09 +0200

    Created Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Class Main
+ *
+ * @author Axel Nilsson (axnion)
+ * @version 1.0
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Working");
+    }
+}
+
+// Created: 2016-04-11
