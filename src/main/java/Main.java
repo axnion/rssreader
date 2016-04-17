df8bad4ef01bd7fcc48cd98584e20c34dd400f8b
Axel Nilsson <an222yp@student.lnu.se>
2016-04-17 14:08:40 +0200

    Created an early implementation of Feed class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,43 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
+import api.Feed;
+import api.Item;
+
+import java.util.Scanner;
 
 /**
  * Class Main
  *
  * @author Axel Nilsson (axnion)
- * @version 1.0
+ * @version 0.1
  */
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Working");
+        System.out.print("URL TO RSS FEED: ");
+        Scanner scan = new Scanner(System.in);
+        String url = scan.nextLine();
+
+        Feed feed = new Feed(url);
+        feed.update();
+
+        System.out.println("TITLE: " + feed.getTitle());
+        System.out.println("LINK: " + feed.getLink());
+        System.out.println("DESCRIPTION: " + feed.getDescription());
+        System.out.println("URLTOXML: " + feed.getUrlToXML());
+        System.out.println("");
+
+        Item[] items = feed.getItems();
+
+        for(Item item : items)
+        {
+            if(item == null)
+                break;
+
+            System.out.println("TITLE: " + item.getTitle());
+            System.out.println("LINK: " + item.getLink());
+            System.out.println("DESCRIPTION: " + item.getDescription());
+            System.out.println("ID: " + item.getId());
+        }
     }
 }
 
