b017ad9bf25b88e1405d65b5d8bdead19d49437f
Axel Nilsson <an222yp@student.lnu.se>
2016-05-13 03:50:11 +0200

    Updated Item with compareName and compareDate. Added implementation, testing and comments

--- src/main/java/api/Item.java
+++ src/main/java/api/Item.java
@@ -56,6 +56,110 @@ public class Item
         this.starred = starred;
     }
 
+    /**
+     * Compares this Items title to another Item objects title, if this Items title is
+     * earlier in the alphabet than the other the value returned is lower than 0. If this items
+     * title is later than others title the returned value is higher than 0. If they are the same 0
+     * is returned
+     * @param other The Item object to compare this Item with
+     * @return      An integer representing the comparison of the titles of these two Items
+     */
+    int compareTitle(Item other)
+    {
+        return this.getTitle().compareTo(other.getTitle());
+    }
+
+    /**
+     * Compares this Items date to another Items title. It will compare year, months, days, hours,
+     * minutes, and seconds, in that order. If the other item is newer than this it will return an
+     * integer smaller than 0. And if this is newer than the other a positive integer is returned.
+     * If they are the same 0 is returned.
+     * @param other The Item object to compare this Item with
+     * @return      An integer representing the comparison of the titles of these two Items
+     */
+    int compareDate(Item other)
+    {
+        int comparison;
+
+        // Comparing years
+        comparison = Integer.parseInt(this.getDate().substring(12, 16)) -
+                Integer.parseInt(other.getDate().substring(12, 16));
+        if(comparison != 0)
+            return comparison;
+
+        // Comparing months
+        comparison = monthStringToInt(this.getDate().substring(8, 11)) -
+                monthStringToInt(other.getDate().substring(8, 11));
+        if(comparison != 0)
+            return comparison;
+
+        // Comparing days
+        comparison = removeFirstZero(this.getDate().substring(5, 7)) -
+                removeFirstZero(other.getDate().substring(5, 7));
+        if(comparison != 0)
+            return comparison;
+
+        // Comparing hours
+        comparison = removeFirstZero(this.getDate().substring(17, 19)) -
+                removeFirstZero(other.getDate().substring(17, 19));
+        if(comparison != 0)
+            return comparison;
+
+        // Comparing minutes
+        comparison = removeFirstZero(this.getDate().substring(20, 22)) -
+                removeFirstZero(other.getDate().substring(20, 22));
+        if(comparison != 0)
+            return comparison;
+
+        // Comparing seconds
+        comparison = removeFirstZero(this.getDate().substring(23, 25)) -
+                removeFirstZero(other.getDate().substring(23, 25));
+        if(comparison != 0)
+            return comparison;
+
+        return 0;
+    }
+
+    /**
+     * Takes a three character String representing a month. If the String matches one of the
+     * alternatives an integer associated with that month.
+     * @param str   A String representing a month.
+     * @return      An integer representing a month.
+     */
+    private int monthStringToInt(String str)
+    {
+        switch (str)
+        {
+            case "Jan": return 1;
+            case "Feb": return 2;
+            case "Mar": return 3;
+            case "Apr": return 4;
+            case "May": return 5;
+            case "Jun": return 6;
+            case "Jul": return 7;
+            case "Aug": return 8;
+            case "Sep": return 9;
+            case "Oct": return 10;
+            case "Nov": return 11;
+            case "Dec": return 12;
+        }
+
+        return 0;
+    }
+
+    /**
+     * Removes the first character if it is a 0.
+     * @param str   A String containing numbers.
+     * @return      An integer converted from the argument
+     */
+    private int removeFirstZero(String str)
+    {
+        if(str.charAt(0) == '0')
+            str = "" + str.charAt(1);
+
+        return Integer.parseInt(str);
+    }
+
     /*
     ------------------------------- ACCESSORS AND MUTATORS -----------------------------------------
     */
--- src/test/java/api/ItemTest.java
+++ src/test/java/api/ItemTest.java
@@ -96,6 +96,276 @@ public class ItemTest
         assertFalse(testItem.isVisited());
         assertFalse(testItem.isStarred());
     }
-}
 
+    /**
+     * Test Case: 72
+     * Checks the result of comparing by title two Items with the same title. It should return a 0.
+     */
+    @Test
+    public void compareTitleEqual()
+    {
+        Item testItem = new Item();
+        Item other = new Item();
+
+        testItem.setTitle("A title");
+        other.setTitle("A title");
+
+        assertEquals(0, testItem.compareTitle(other));
+    }
+
+    /**
+     * Test Case: 73
+     * Checks the result of comparing by title two Items where the other Item is later
+     * alphabetically and has a higher value. The return should be smaller than 0.
+     */
+    @Test
+    public void compareTitleOtherHigher()
+    {
+        Item testItem = new Item();
+        Item other = new Item();
+
+        testItem.setTitle("A title");
+        other.setTitle("B title");
+
+        assertTrue(testItem.compareTitle(other) < 0);
+    }
+
+    /**
+     * Test Case: 74
+     * Checks the result of comparing by title two Items where the other Item is earlier
+     * alphabetically and has a smaller value. The return should be larger than 0.
+     */
+    @Test
+    public void compareTitleOtherLower()
+    {
+        Item testItem = new Item();
+        Item other = new Item();
+
+        testItem.setTitle("B title");
+        other.setTitle("A title");
+
+        assertTrue(testItem.compareTitle(other) > 0);
+    }
+
+    /**
+     * Test Case: 75
+     * Checks the result of comparing two Items by date. Both dates are the same so the returned
+     * value should be 0.
+     */
+    @Test
+    public void compareDateEqual()
+    {
+        Item testItem = new Item();
+        Item other = new Item();
+
+        testItem.setDate("Mon, 01 Jan 2016 12:00:00 +0000");
+        other.setDate("Mon, 01 Jan 2016 12:00:00 +0000");
+
+        assertEquals(0, testItem.compareDate(other));
+    }
+
+    /**
+     * Test Case: 76
+     * Checks the result of comparing two Items by date. The other Items years is later than this
+     * objects so the result should be smaller than 0.
+     */
+    @Test
+    public void compareDateOtherYearHigher()
+    {
+        Item testItem = new Item();
+        Item other = new Item();
+
+        testItem.setDate("Mon, 01 Jan 2015 12:00:00 +0000");
+        other.setDate("Mon, 01 Jan 2016 12:00:00 +0000");
+
+        assertTrue(testItem.compareDate(other) < 0);
+    }
+
+    /**
+     * Test Case: 77
+     * Checks the result of comparing two Items by date. The other Items months is later than this
+     * objects so the result should be smaller than 0.
+     */
+    @Test
+    public void compareDateOtherMonthHigher()
+    {
+        Item testItem = new Item();
+        Item other = new Item();
+
+        testItem.setDate("Mon, 01 Jan 2016 12:00:00 +0000");
+        other.setDate("Mon, 01 Feb 2016 12:00:00 +0000");
+
+        assertTrue(testItem.compareDate(other) < 0);
+    }
+
+    /**
+     * Test Case: 78
+     * Checks the result of comparing two Items by date. The other Items days is later than this
+     * objects so the result should be smaller than 0.
+     */
+    @Test
+    public void compareDateOtherDayHigher()
+    {
+        Item testItem = new Item();
+        Item other = new Item();
+
+        testItem.setDate("Mon, 01 Jan 2016 12:00:00 +0000");
+        other.setDate("Mon, 05 Jan 2016 12:00:00 +0000");
+
+        assertTrue(testItem.compareDate(other) < 0);
+    }
+
+    /**
+     * Test Case: 79
+     * Checks the result of comparing two Items by date. The other Items hours is later than this
+     * objects so the result should be smaller than 0.
+     */
+    @Test
+    public void compareDateOtherHourHigher()
+    {
+        Item testItem = new Item();
+        Item other = new Item();
+
+        testItem.setDate("Mon, 01 Jan 2016 12:00:00 +0000");
+        other.setDate("Mon, 01 Jan 2016 13:00:00 +0000");
+
+        assertTrue(testItem.compareDate(other) < 0);
+    }
+
+    /**
+     * Test Case: 80
+     * Checks the result of comparing two Items by date. The other Items minutes is later than this
+     * objects so the result should be smaller than 0.
+     */
+    @Test
+    public void compareDateOtherMinuteHigher()
+    {
+        Item testItem = new Item();
+        Item other = new Item();
+
+        testItem.setDate("Mon, 01 Jan 2016 12:00:00 +0000");
+        other.setDate("Mon, 01 Jan 2016 12:15:00 +0000");
+
+        assertTrue(testItem.compareDate(other) < 0);
+    }
+
+    /**
+     * Test Case: 81
+     * Checks the result of comparing two Items by date. The other Items seconds is later than this
+     * objects so the result should be smaller than 0.
+     */
+    @Test
+    public void compareDateOtherSecondHigher()
+    {
+        Item testItem = new Item();
+        Item other = new Item();
+
+        testItem.setDate("Mon, 01 Jan 2016 12:00:00 +0000");
+        other.setDate("Mon, 01 Jan 2016 12:00:20 +0000");
+
+        assertTrue(testItem.compareDate(other) < 0);
+    }
+
+    /**
+     * Test Case: 82
+     * Checks the result of comparing two Items by date. The other Items years is earlier than this
+     * objects so the result should be smaller than 0.
+     */
+    @Test
+    public void compareDateOtherYearLower()
+    {
+        Item testItem = new Item();
+        Item other = new Item();
+
+        testItem.setDate("Mon, 01 Jan 2016 12:00:00 +0000");
+        other.setDate("Mon, 02 Jan 2015 12:00:00 +0000");
+
+        assertTrue(testItem.compareDate(other) > 0);
+    }
+
+    /**
+     * Test Case: 83
+     * Checks the result of comparing two Items by date. The other Items months is earlier than this
+     * objects so the result should be smaller than 0.
+     */
+    @Test
+    public void compareDateOtherMonthLower()
+    {
+        Item testItem = new Item();
+        Item other = new Item();
+
+        testItem.setDate("Mon, 01 Aug 2016 12:00:00 +0000");
+        other.setDate("Mon, 02 Jan 2016 12:00:00 +0000");
+
+        assertTrue(testItem.compareDate(other) > 0);
+    }
+
+    /**
+     * Test Case: 84
+     * Checks the result of comparing two Items by date. The other Items days is earlier than this
+     * objects so the result should be smaller than 0.
+     */
+    @Test
+    public void compareDateOtherDayLower()
+    {
+        Item testItem = new Item();
+        Item other = new Item();
+
+        testItem.setDate("Mon, 10 Jan 2016 12:00:00 +0000");
+        other.setDate("Mon, 05 Jan 2016 12:00:00 +0000");
+
+        assertTrue(testItem.compareDate(other) > 0);
+    }
+
+    /**
+     * Test Case: 85
+     * Checks the result of comparing two Items by date. The other Items hours is earlier than this
+     * objects so the result should be smaller than 0.
+     */
+    @Test
+    public void compareDateOtherHourLower()
+    {
+        Item testItem = new Item();
+        Item other = new Item();
+
+        testItem.setDate("Mon, 01 Jan 2016 12:00:00 +0000");
+        other.setDate("Mon, 01 Jan 2016 10:00:00 +0000");
+
+        assertTrue(testItem.compareDate(other) > 0);
+    }
+
+    /**
+     * Test Case: 86
+     * Checks the result of comparing two Items by date. The other Items minutes is earlier than this
+     * objects so the result should be smaller than 0.
+     */
+    @Test
+    public void compareDateOtherMinuteLower()
+    {
+        Item testItem = new Item();
+        Item other = new Item();
+
+        testItem.setDate("Mon, 01 Jan 2016 12:15:00 +0000");
+        other.setDate("Mon, 01 Jan 2016 12:00:00 +0000");
+
+        assertTrue(testItem.compareDate(other) > 0);
+    }
+
+    /**
+     * Test Case: 87
+     * Checks the result of comparing two Items by date. The other Items seconds is earlier than this
+     * objects so the result should be smaller than 0.
+     */
+    @Test
+    public void compareDateOtherSecondLower()
+    {
+        Item testItem = new Item();
+        Item other = new Item();
+
+        testItem.setDate("Mon, 01 Jan 2016 12:00:15 +0000");
+        other.setDate("Mon, 01 Jan 2016 12:00:00 +0000");
+
+        assertTrue(testItem.compareDate(other) > 0);
+    }
+}
 // Created: 2016-04-17
