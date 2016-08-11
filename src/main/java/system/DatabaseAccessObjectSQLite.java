4eb6f2d7740fb1e64b2200a897c94662695a0cc6
Axel Nilsson <an222yp@student.lnu.se>
2016-08-11 02:26:00 +0200

    Added small comment to DatabaseAccessObjectSQLite

--- src/main/java/system/DatabaseAccessObjectSQLite.java
+++ src/main/java/system/DatabaseAccessObjectSQLite.java
@@ -15,6 +15,8 @@ import java.util.Date;
 /**
  * Class DatabaseAccessObjectSQLite
  *
+ * This class is used to load and save user data in the RSSReader application.
+ *
  * @author Axel Nilsson (axnion)
  */
 class DatabaseAccessObjectSQLite {
