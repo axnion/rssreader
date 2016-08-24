521cbaa7a09fc12f8e8686006b6ceb4090fb6046
Axel Nilsson <an222yp@student.lnu.se>
2016-08-24 15:42:14 +0200

    Small changes to build file

--- src/main/java/system/Configuration.java
+++ src/main/java/system/Configuration.java
@@ -103,6 +103,7 @@ public class Configuration {
         executorService = Executors.newSingleThreadScheduledExecutor();
         executorService.scheduleAtFixedRate(new Thread() {
             public void run() {
+                setDaemon(true);
                 Thread.currentThread().setName("UpdaterThread");
                 boolean updated = false;
                 for(FeedList feedList : feedLists) {
